1d86df8a04230dd4d92586e0ac71b4f1ab26b0b9
foxamanda2 <fox.amanda2@gmail.com>
2021-02-22 17:04:34 -0500

    Color adjust working

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,64 @@
 import React, { Component } from 'react'
 
-// CSS
-// const styles = styled.div{
-
-// }
-export class Slider extends Component {
-  state = {
-    value: 0,
-  }
-
-  handleOnChange = event => {
-    return this.setState({ value: event.target.value })
-  }
-  render() {
-    return (
-      <div>
-        <input
-          className="slider"
-          type="range"
-          min={0}
-          max={255}
-          value={this.state.value}
-          onChange={this.handleOnChange}
-        />
-        <div className="value">{this.state.value}</div>
-      </div>
-    )
-  }
-}
 export class App extends Component {
-  handleUpdateColor = () => {
-    const red = this.state.color
-  }
   state = {
-    color: '000000',
+    hue: Math.floor(Math.random() * 255),
+    saturation: Math.floor(Math.random() * 255),
+    light: Math.floor(Math.random() * 255),
+  }
+
+  handleOnChangeHue = event => {
+    this.setState({ hue: event.target.value })
+    console.log(this.state.hue)
+  }
+  handleOnChangeSaturation = event => {
+    this.setState({ saturation: event.target.value })
+    console.log(this.state.saturation)
+  }
+  handleOnChangeLight = event => {
+    this.setState({ light: event.target.value })
+    console.log(this.state.light)
   }
 
   render() {
     return (
       <div>
         <h1>Color Picker</h1>
-        <h2>
-          <Slider
-            color="#0076D9"
-            hexColor="red"
-            handleUpdateColor={this.HandleUpdateColor}
+        <div
+          className="ColorBox"
+          style={{
+            backgroundColor: `hsl(${this.state.hue},${this.state.saturation}%,${this.state.light}%)`,
+          }}
+        ></div>
+        <div>
+          <input
+            className="hue-slider"
+            type="range"
+            min={0}
+            max={255}
+            value={this.state.hue}
+            onChange={this.handleOnChangeHue}
           />
-          <Slider
-            color="#0076D9"
-            hexColor="red"
-            handleUpdateColor={this.HandleUpdateColor}
+          <div className="hue">{this.state.hue}</div>
+          <input
+            className="saturation-slider"
+            type="range"
+            min={0}
+            max={255}
+            value={this.state.saturation}
+            onChange={this.handleOnChangeSaturation}
           />
-          <Slider
-            color="#0076D9"
-            hexColor="red"
-            handleUpdateColor={this.HandleUpdateColor}
+          <div className="saturation">{this.state.saturation}</div>
+          <input
+            className="light-slider"
+            type="range"
+            min={0}
+            max={255}
+            value={this.state.light}
+            onChange={this.handleOnChangeLight}
           />
-        </h2>
+          <div className="light">{this.state.light}</div>
+        </div>
       </div>
     )
   }
